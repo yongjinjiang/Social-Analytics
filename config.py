5ef74e0a2c3b4f50ad24c2ec99affaf5c857620d
Yongjin <yjjiangphysics@gmail>
2018-12-25 01:30:37 -0600

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
+
+
+consumer_key="yZPuxugb3j3psgGZ7wu6zxLfH" # (API key)
+
+consumer_secret="by4Gt5X0J6ltEssENxhI5FOJK5BxBZOh6BqSoboCA0vVEVvlp1"
+
+access_token="1029867143502946309-vuRD1U6gFyFHjXvcltwkbhI2Yjfi1F"  #(Access token)
+
+access_token_secret="ieDK29OKQz2RUiozz6JcZw4Yx3ZVSIAFWMiqhngkSc34i" # (Access token secret)
